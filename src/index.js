48bd26e3ec1462ec710114018d2f99776a430350
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2020-07-12 20:05:42 +0200

    Hello World from re:dom!

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log("Hello World!");
+import { el, mount } from "redom";
+import audioSrc from "../media/Brahmsee/01.opus";
+
+class Audio {
+    constructor(src) {
+        this.el = el("audio", {controls:true},
+                     this.source = el("source", {
+                         src,
+                         type:"audio/ogg"}));
+        this.data = {};
+    }
+
+    update(data) {
+        const {src} = data;
+
+        if (src != this.data.src) {
+            this.source.setAttribute("src", src);
+        }
+
+        this.data = data;
+    }
+}
+
+const audio = new Audio(audioSrc);
+//audio.update({src: audioSrc});
+mount(document.body, audio);
